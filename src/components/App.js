5c6b7442254f70cb2e21f6e0b687ff8c5a277032
ChristianSama <christiansama1601@gmail.com>
2022-02-19 03:05:22 -0500

    replaced class components to function components in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,30 @@
-import { Component } from "react";
+import React, { useState, useEffect } from 'react';
 import GeneralSection from "./GeneralSection"
 import EducationSection from "./EducationSection"
 import ExperienceSection from "./ExperienceSection"
 
-class App extends Component {
+function App() {
+  const [isEditing, setIsEditing] = useState(true);
 
-  constructor(props) {
-    super(props);
-    this.state = {edit: true}
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(event) {
+  function handleSubmit(event) {
     event.preventDefault();
-    this.setState(prevState => ({
-      edit: !prevState.edit
-    })); 
+    setIsEditing(!isEditing);
   }
 
-  render() {
-    const formButtonValue = this.state.edit ? 'View' : 'Edit';
-    return (
-      <div className="app">
-        <h1>CV visualizer</h1>
-        <form onSubmit={this.handleSubmit}>
-          <h2>General Information</h2>
-          <GeneralSection edit={this.state.edit}/>
-          <h2>Education</h2>
-          <EducationSection edit={this.state.edit}/>
-          <h2>Work Experience</h2>
-          <ExperienceSection edit={this.state.edit}/>
-          <button type="submit" value={formButtonValue}>{formButtonValue}</button>
-        </form>
-      </div>
-    );
-  }
+  const formButtonValue = isEditing ? 'View' : 'Edit';
+
+  return <div className="app">
+            <h1>CV visualizer</h1>
+            <form onSubmit={handleSubmit}>
+              <h2>General Information</h2>
+              <GeneralSection edit={isEditing}/>
+              <h2>Education</h2>
+              <EducationSection edit={isEditing}/>
+              <h2>Work Experience</h2>
+              <ExperienceSection edit={isEditing}/>
+              <button type="submit" value={formButtonValue}>{formButtonValue}</button>
+            </form>
+          </div>
 }
 
 export default App;
